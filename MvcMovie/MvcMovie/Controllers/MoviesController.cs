2bc46d14dad136fd0b7b034e79f3238e859a79f7
mark-savik <99361212+mark-savik@users.noreply.github.com>
2022-07-24 02:04:21 +0200

    filter/search added

--- MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -20,21 +20,43 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string searchString)
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                orderby m.Genre
+                select m.Genre;
+            var movies = from m in _context.Movie
+                select m;
 
-            var movies = from m in _context.Movie select m;
-
-            if (!String.IsNullOrEmpty(searchString))
+            if (!string.IsNullOrEmpty(searchString))
             {
                 movies = movies.Where(s => s.Title!.Contains(searchString));
             }
-            return View(await movies.ToListAsync());
 
-            //return _context.Movie != null ? 
-            //            View(await _context.Movie.ToListAsync()) :
-            //            Problem("Entity set 'MvcMovieContext.Movie'  is null.");
-        }
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreViewModel = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreViewModel);
+        
+        //return _context.Movie != null ? 
+        //            View(await _context.Movie.ToListAsync()) :
+        //            Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+    }
+
+        //[HttpPost]
+        //public string Index(string searchString, bool notUsed)
+        //{
+        //    return "From [HttpPost]Index: filter on " + searchString;
+        //}
 
         // GET: Movies/Details/5
         public async Task<IActionResult> Details(int? id)
