6ee40f05aee02ebc6b0d91491bb535da229508f7
mark-savik <99361212+mark-savik@users.noreply.github.com>
2022-08-17 01:54:41 +0200

    fake implementation

--- CalculatorForTest/CalculatorForTest/Program.cs
+++ CalculatorForTest/CalculatorForTest/Program.cs
@@ -1,2 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CalculatorForTest;
+using CalculatorForTest.Logs;
+
+Calculator calculator = new();
+LogManager logManager = new(new ReadWriteLog());
+logManager.LoadLogs("logs.txt");
+
+while (true)
+{
+    Console.Clear();
+    Console.WriteLine("Witaj w kalkulatorze");
+    Console.WriteLine("1. Dodaj");
+    Console.WriteLine("2. Odejmij");
+    Console.WriteLine("3. Pomnóż");
+    Console.WriteLine("4. Podziel");
+    Console.WriteLine("0. Wyjście");
+
+    string input = Console.ReadLine();
+    if (!int.TryParse(input, out int option))
+    {
+        Console.WriteLine("Wybrałęś nieprawidłową opcję");
+        continue;
+    }
+
+    double first, second;
+    double result;
+    if (option == 0)
+    {
+        break;
+    }
+
+    Console.WriteLine("Podaj 1szą liczbę");
+    string firstCandidate = Console.ReadLine();
+    if (!double.TryParse(firstCandidate, out first))
+    {
+        Console.WriteLine("Nieprawidłowy format liczby");
+        continue;
+    }
+
+    Console.WriteLine("Podaj 2gą liczbę");
+    string secondCandidate = Console.ReadLine();
+    if (!double.TryParse(secondCandidate, out second))
+    {
+        Console.WriteLine("Nieprawidłowy format liczby");
+        continue;
+    }
+
+    switch (option)
+    {
+        case 1:
+            result = calculator.Add(first, second);
+            Console.WriteLine($"Wynik dodawania {first} oraz {second} wynosi {result}");
+            break;
+        case 2:
+            result = calculator.Substract(first, second);
+            Console.WriteLine($"Wynik odejmowania {second} od {first} wynosi {result}");
+            break;
+        case 3:
+            result = calculator.Multiply(first, second);
+            Console.WriteLine($"Wynik mnożenia {first} przez {second} wynosi {result}");
+            break;
+        case 4:
+            try
+            {
+                result = calculator.Divide(first, second);
+                Console.WriteLine($"Wynik to {result}");
+                logManager.AddLog($"Wynik dzielenia {first} i {second} to {result}");
+            }
+            catch 
+            {
+                logManager.AddLog("Próba dzielenia przez 0");
+            }
+            break;
+        default:
+            break;
+    }
+
+    Console.ReadKey();
+}
+logManager.Save("logs.txt");
+Console.WriteLine("Dzięki za liczenie");
+Console.ReadKey();
---
+++ CalculatorForTest/UnitTestProject1/LogManagerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalculatorForTest.Logs;
+using FluentAssertions;
+
+namespace CalculatorForTest.Tests
+{
+    public class LogManagerTests
+    {
+        [Fact]
+        public void LoadLogs_WhenReadWriteLogLoadsLogs_ListNotEmpty()
+        {
+            LogManager logManager = new(new ReadWriteLogFake());
+            logManager.LoadLogs("fakepath");
+            logManager.Logs.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void AddLogs_WithMessage_LogAdded()
+        {
+            LogManager logManager = new(new ReadWriteLogFake());
+            logManager.AddLog("testmessage");
+            logManager.Logs.Count.Should().Be(1);
+        }
+    }
+}
